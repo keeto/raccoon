4b6b7a9af1d6498b3906fe268fe74c3d369b1235
Keeto Obcena <markeeto@gmail.com>
2009-07-15 17:29:56 +0800

    Added output buffering to take care of headers.

--- app/controllers/default.js
+++ app/controllers/default.js
@@ -5,13 +5,13 @@ this.Default = new Class({
 	Extends: Controller,
 
 	index: function(){
-		this.render('common/welcome').echo();
+		this.puts(this.render('common/welcome'));
 	},
 
 	hello: function(name){
 		name = name || "World";
 		this.greeting = "Hello " + name.capitalize() + "!";
-		this.render('common/helloworld').echo();
+		this.puts(this.render('common/helloworld'));
 	}
 
 });
